d289b2d4a39a1f8cebb8b8eb3a2d9845289f1650
talhacoder302 <talha.developments@gmail.com>
2026-07-23 12:21:35 +0500

    chore: initialize backend project structure and global path aliases

---
+++ backend/app-global.js
@@ -0,0 +1,13 @@
+global.__root = __dirname;
+global.__base = `${__root}/src`;
+
+global.__config = `${__base}/config`;
+global.__controller = `${__base}/controller`;
+global.__models = `${__base}/models`;
+global.__routes = `${__base}/routes`;
+global.__utils = `${__base}/utils`;
+global.__middelwares = `${__base}/middelwares`;
+global.__scripts = `${__base}/scripts`;
+global.__views = `${__base}/views`;
+global.__public = `${__root}/public`;
+global.__uploads = `${__root}/uploads`;
